0bc9ea8e2f4f836873a8fd6b1a13278e0cd97b5c
Sam Tey <samazingg96@gmail.com>
2024-01-11 15:40:00 +0800

    feat: develop animation for modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,6 @@
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useLayoutEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
+import { cc } from "../utils/cc";
 
 export type ModalProps = {
     children: ReactNode;
@@ -8,6 +9,11 @@ export type ModalProps = {
 };
 
 export function Modal({ children, isOpen, onClose }: ModalProps) {
+    //the process of closing
+    const [isClosing, setIsClosing] = useState(false);
+
+    const prevIsOpen = useRef<boolean>();
+
     useEffect(() => {
         function handler(e: KeyboardEvent) {
             if (e.key === "Escape") onClose();
@@ -19,9 +25,30 @@ export function Modal({ children, isOpen, onClose }: ModalProps) {
             document.removeEventListener("keydown", handler);
         };
     }, [onClose]);
-    if (!isOpen) return null;
+
+    /**
+     * if we actually change our isOpen and prevIsOpen, we have to make sure this goes into effect
+     * before Modal component re-render
+     * Will run useLayoutEffect first before browser repaint the UI
+     * Run before the re-render, usually use for animation
+     * Designed to handle side effects that require immediate DOM layout updates
+     * https://blog.logrocket.com/react-useeffect-vs-uselayouteffect-hooks-examples/
+     */
+    useLayoutEffect(() => {
+        //went from open to close
+        if (!isOpen && prevIsOpen.current) {
+            setIsClosing(true);
+        }
+        prevIsOpen.current = isOpen;
+    }, [isOpen]);
+
+    if (!isOpen && !isClosing) return null;
+
     return createPortal(
-        <div className="modal">
+        <div
+            onAnimationEnd={() => setIsClosing(false)}
+            className={cc("modal", isClosing && "closing")}
+        >
             <div className="overlay" onClick={onClose}></div>
             <div className="modal-body">{children}</div>
         </div>,
